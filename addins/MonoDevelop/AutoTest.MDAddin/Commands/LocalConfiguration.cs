568c98941249327938c5591e6546f25ffb4d301e
Eberhard Beilharz <eb1@sil.org>
2013-10-10 12:42:32 +0200

    Allow creating of local config file
    It is now possible to create a solution specific configuration file
    through the menu item. Previously you could only edit an existing
    config file.

--- addins/MonoDevelop/AutoTest.MDAddin/Commands/LocalConfiguration.cs
+++ addins/MonoDevelop/AutoTest.MDAddin/Commands/LocalConfiguration.cs
@@ -2,6 +2,7 @@
 // Distributable under the terms of the MIT license (http://opensource.org/licenses/MIT).
 using System;
 using System.IO;
+using System.Reflection;
 using MonoDevelop.Components.Commands;
 using MonoDevelop.Ide;
 
@@ -12,20 +13,29 @@ namespace AutoTest.MDAddin.Commands
 		protected override void Run()
 		{
 			var configFile = LocalConfigFile;
+			if (string.IsNullOrEmpty(configFile))
+				return;
+
 			if (File.Exists(configFile))
 				IdeApp.Workbench.OpenDocument(configFile);
+			else
+			{
+				var assembly = Assembly.GetExecutingAssembly();
+				var stream = assembly.GetManifestResourceStream("AutoTest.MDAddin.Resources.AutoTest.config.template.MD");
+				IdeApp.Workbench.NewDocument(LocalConfigFile, "application/xml", stream);
+			}
 		}
 
 		protected override void Update(CommandInfo info)
 		{
-			info.Enabled = File.Exists(LocalConfigFile);
+			info.Enabled = (IdeApp.ProjectOperations.CurrentSelectedSolution != null);
 		}
 
 		private string LocalConfigFile
 		{
 			get
 			{
-				if (IdeApp.ProjectOperations.CurrentSelectedSolution != null && IdeApp.ProjectOperations.CurrentSelectedSolution.BaseDirectory != null)
+				if (IdeApp.ProjectOperations.CurrentSelectedSolution != null)
 					return Path.Combine(IdeApp.ProjectOperations.CurrentSelectedSolution.BaseDirectory.ToString(), "AutoTest.config");
 				return string.Empty;
 			}
