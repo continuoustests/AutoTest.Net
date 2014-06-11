2793695908d52483314937b84b40fd08675a3898
Eberhard Beilharz <eb1@sil.org>
2014-06-11 10:21:24 +0200

    Revert "Fix build for MD 4.0"
    This reverts commit a4fbd7d12c2f08732bb0dab6c5105e0aea310f38.
    Since the current MD version is now 5.0 we don't need this
    anymore.

--- addins/MonoDevelop/AutoTest.MDAddin/ContinuousTestsPad.cs
+++ addins/MonoDevelop/AutoTest.MDAddin/ContinuousTestsPad.cs
@@ -474,20 +474,14 @@ namespace AutoTest.MDAddin
 
 		internal void OnDebugTest()
 		{
-#if MD_41
 			var debugModeSet = Runtime.ProcessService.GetDebugExecutionMode();
 			var mode = debugModeSet.ExecutionModes.First();
 			RunSelectedTest(mode.ExecutionHandler);
-#endif
 		}
 
 		internal void OnUpdateDebugTest(CommandInfo info)
 		{
-#if MD_41
 			OnUpdateRunTest(info);
-#else
-			info.Enabled = false;
-#endif
 		}
 
 		#endregion
